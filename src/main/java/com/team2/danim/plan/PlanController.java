12d458be0d63c43c27be2c97936e94af4f7c92e0
hyeah <99235510+hye-ah@users.noreply.github.com>
2022-03-03 10:03:08 +0900

    Hap 22 (#49)
    * 0228 plan_map
    
    * 0302 - plan
    
    Co-authored-by: ahyosubkima <90039038+ahyosubkima@users.noreply.github.com>

--- src/main/java/com/team2/danim/plan/PlanController.java
+++ src/main/java/com/team2/danim/plan/PlanController.java
@@ -2,12 +2,19 @@ package com.team2.danim.plan;
 
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.team2.danim.TokenMaker;
+
 @Controller
 public class PlanController {
+	
+	@Autowired
+	private PlanDAO pDAO;
 	
 	@RequestMapping(value = "/planMain", method = RequestMethod.GET)
 
@@ -22,6 +29,18 @@ public class PlanController {
 	public String planWrite(HttpServletRequest req) {
 		
 		req.setAttribute("contentPage", "plan/planWrite.jsp");
+		return "home";
+
+	}
+	
+	
+	@RequestMapping(value = "/plan.detailPlanner", method = RequestMethod.POST)
+	public String planDetail(HttpServletRequest req) {
+		
+		TokenMaker.make(req);
+		pDAO.upload(req);
+		
+		req.setAttribute("contentPage", "plan/planDetail.jsp");
 		return "home";
 
 	}

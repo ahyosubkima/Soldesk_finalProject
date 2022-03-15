9464134b26745f243ef4353b3905f4459a6af569
ahyosubkima <moneycf3@gmail.com>
2022-03-15 15:41:00 +0900

    3.15 reviewWrite 테이블, 데일리 추가드롭버튼작업

--- src/main/webapp/resources/review/js/reviewWrite.js
+++ src/main/webapp/resources/review/js/reviewWrite.js
@@ -33,6 +33,69 @@ document.getElementById('dailyAddBtn').addEventListener('click',function(){
    // console.log(clone);
 
 })
+
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+if(event.target.classList.contains('dropbtn')){
+    console.log(event.target.parentNode.childNodes[3].classList.toggle("show"));
+}
+
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+
+  if(event.target.classList.contains('insert_col_back')){
+ createSchedule(event.target);
+  }
+}
+
+
 	
 });
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+// function doDropdown() {
+    
+//     document.getElementById("myDropdown").classList.toggle("show");
+
+
+// }
+
+let daycount = 1;
+
+function createSchedule(a){
+
+   let newDiv =  document.createElement('div');
+   newDiv.setAttribute('id','daily_schedule_con');
+
+   ++daycount;
+
+   newDiv.innerHTML = '<div class="dropdown">\
+       <button onclick="" class="dropbtn">\
+           <img alt="" src="resources/review/img/plus-circle.svg">\
+       </button>\
+       <div id="myDropdown" class="dropdown-content">\
+           <a href="javascript:void(0);" onclick="createSchedule()">앞에 일정추가</a> <a href="javascript:void(0);" class="insert_col_back">뒤에 일정추가</a> <a href="#">일정삭제</a>\
+       </div>\
+   </div>\
+   <a href="#"><span>day '+daycount+'</span></a>'
+   console.log(newDiv);
+   console.log(a.parentNode.parentNode.parentNode);
+   //document.getElementById('schedule_inner_nav').inn
+//    a.parentNode.parentNode.parentNode.append('afterend',newDiv);
+    a.parentNode.parentNode.parentNode.parentNode.append(newDiv);
+    
+
+
+}
 

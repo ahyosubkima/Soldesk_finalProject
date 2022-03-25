6626cb8f4efd67b8f5eda10822f529844515dc8e
Byeong Gwan Seo <89896745+Gomserker@users.noreply.github.com>
2022-03-25 17:59:58 +0900

    Bg register 0317 (#62)
    * 서병관 3/22 작업
    
    * 0323 개같은 메일링ㅁ낭러ㅛㅐ멷뇨ㅐㄱ8ㅈㅁ데98갸먀ㅕㅔㄴ;ㅐ댜ㅕ레;ㄴㅁㄹ\
    
    * 22.03.25

--- src/main/java/com/team2/danim/member/MemberController.java
+++ src/main/java/com/team2/danim/member/MemberController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MemberController {
+	
 	@Autowired
 	private MemberDAO mDAO;
-	
 
 	//로그인 처리
 
@@ -47,12 +47,14 @@ public class MemberController {
 		return "home";
 	}
 	
+	//마이페이지 이동
 	@RequestMapping(value = "member.myPage", method = RequestMethod.GET)
 	public String toMyPage(Member m, HttpServletRequest req) {
 		req.setAttribute("contentPage", "member/myPage.jsp");
 		return "home";
 	}
 	
+	//회원 탈퇴
 	@RequestMapping(value = "member.delete", method = RequestMethod.GET)
 	public String deleteMember(HttpServletRequest req) {
 		
@@ -62,6 +64,7 @@ public class MemberController {
 		return "home";
 	}
 	
+	//회원 정보 수정 페이지 이동
 	@RequestMapping(value = "member.update", method = RequestMethod.GET)
 	public String toMemberUpdate(Member m, HttpServletRequest req) {
 		
@@ -69,7 +72,7 @@ public class MemberController {
 		return "home";
 	}
 	
-	
+	//회원 정보 수정
 	@RequestMapping(value = "member.updateInfo", method = RequestMethod.POST)
 	public String MemberUpdate(Member m, HttpServletRequest req) {
 		mDAO.updateMember(m, req);
@@ -77,6 +80,7 @@ public class MemberController {
 		return "home";
 	}
 	
+	//아이디 중복 체크
 	@RequestMapping(value = "member.idCheck", method = RequestMethod.GET)
 	@ResponseBody
 	public int idCheck(@RequestParam("dm_id") String dm_id) {
@@ -84,4 +88,58 @@ public class MemberController {
 		return mDAO.userIdCheck(dm_id);
 	}
 	
+	//로그인 아이디 체크
+	@RequestMapping(value = "member.idExist", method = RequestMethod.POST)
+	@ResponseBody
+	public int idExist(@RequestParam("dm_id") String dm_id) {
+		
+		return mDAO.userIdExist(dm_id);
+	}
+	
+	//로그인 비번 체크
+	@RequestMapping(value = "member.pwExist", method = RequestMethod.POST)
+	@ResponseBody
+	public int pwExist(@RequestParam("dm_id") String dm_id, @RequestParam("dm_pw") String actual_pw) {
+		
+		return mDAO.userPwExist(dm_id, actual_pw);
+	}
+	
+	//닉네임 중복 체크
+	@RequestMapping(value = "member.nickCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public int nickCheck(@RequestParam("dm_nickname") String dm_nickname) {
+		
+		return mDAO.userNickCheck(dm_nickname);
+	}
+	
+	//이메일 중복 체크
+	@RequestMapping(value = "member.mailCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public int mailCheck(@RequestParam("dm_email") String dm_email) {
+		
+		return mDAO.userMailCheck(dm_email);
+	}
+	
+	//정보찾기 창 열기
+	@RequestMapping(value = "member.findInfo", method = RequestMethod.GET)
+	public String toFindInfo(HttpServletRequest req) {
+		
+		return "member/findInfo";
+	}
+	
+	//id찾기
+	@RequestMapping(value = "member.findId", method = RequestMethod.GET)
+	@ResponseBody
+	public String findId(@RequestParam("dm_email") String dm_email) {
+		
+		return mDAO.findId(dm_email);
+	}
+	
+	//비밀번호 찾기
+	@RequestMapping(value = "member.findPw", method = RequestMethod.GET)
+	@ResponseBody
+	public int findPw(Member m) {
+		
+		return mDAO.findPw(m);
+	}
 }
--- src/main/webapp/resources/db.sql
+++ src/main/webapp/resources/db.sql
@@ -11,13 +11,23 @@ dm_nickname varchar2(20 char) not null,
 dm_email varchar2(300 char) not null,
 dm_addr varchar2(20 char),
 dm_photo varchar2(40 char),
-dm_isAdmin varchar2(2 char) not null 
+dm_isAdmin varchar2(2 char)
 );
 
-insert into danim_member values ('admin2', '1004', '곰', 'abc@abc.abc', '서울', 'dog4.jpg', 'Y');
+/*0322 서병관
+ * 관리자 권한 부여하는 기능 넣으실 분들은
+ * 아래행 insert 하시고 로그인 하시면 됩니다.
+ * id: dm_admin
+ * pw: 1234
+ * jsp에서는 보안 측면에서 hidden 해도 취약하다고 판단해 삭제합니다.
+ * 
+ * */
+insert into danim_member values ('dm_admin', 'ecd102bd4d2c50cb22cdae4ed7c7a395a22f0dd5b448db3b36224bbcd61a8320', '어드민', 'abc@abc.abc', '서울', 'dog4.jpg', 'Y');
+
+
 insert into danim_member
 		values (#{dm_id}, #{dm_pw},
-		#{dm_nickname}, #{dm_email}, #{dm_addr, jdbcType=VARCHAR}, #{dm_photo, jdbcType=VARCHAR}, #{dm_isAdmin})
+		#{dm_nickname}, #{dm_email}, #{dm_addr, jdbcType=VARCHAR}, #{dm_photo, jdbcType=VARCHAR}, #{dm_isAdmin, jdbcType=VARCHAR})
 
 /*insert into danim_member values('jh','1004','김진현','김진지','대구','dog4.jpg')
 insert into danim_member values('bg1','1004','서병관','병관띠','대구','dog.jpg')*/
